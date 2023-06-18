c2b1c904e722608eeea0594413fd84775fdaa99b
oguzhannaydogdu <136969869+oguzhannaydogdu@users.noreply.github.com>
2023-06-18 14:17:52 +0300

    Add files via upload

---
+++ UIKitAutoLayout/UIKitAutoLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitAutoLayoutApp.swift
+//  UIKitAutoLayout
+//
+//  Created by Oğuzhan Aydoğdu on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct UIKitAutoLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
